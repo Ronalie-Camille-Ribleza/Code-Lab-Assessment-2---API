3bec9a4a818753bd14d1df273f124742db327022
Ronalie-Camille-Ribleza <84764554+Ronalie-Camille-Ribleza@users.noreply.github.com>
2023-01-06 02:00:02 +0400

    Swapped icon and header text colors

--- Meal Randomizer/Meal_Randomizer.py
+++ Meal Randomizer/Meal_Randomizer.py
@@ -32,7 +32,7 @@ icon = tk.Label(root,
 icon.grid(row = 0, column = 0, pady = 10, stick = "WE")
 title = tk.Label(text = "Meal Randomizer",
                  bg = "#F9EDCC",
-                 fg = "#EDAE49",
+                 fg = "#FD7168",
                  font = ("Ubuntu Bold", 80))
 title.grid(row = 1, column = 0, pady = 20, stick="WE")
 
